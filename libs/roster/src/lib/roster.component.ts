aa813d35eb13a063d5acdc542fb3473c4aff3a82
Deepak Yadav <deepakyadav991104@gmail.com>
2024-06-14 11:34:18 +0000

    Final changes for this Roster Tab

--- libs/roster/src/lib/roster.component.ts
+++ libs/roster/src/lib/roster.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { UntilDestroy } from '@ngneat/until-destroy';
+import { Component, OnInit } from '@angular/core';
+import { RosterService } from '../data-access/roster.service';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
 
+@UntilDestroy()
 @Component({
   selector: 'realworld-roster',
   templateUrl: './roster.component.html',
   styleUrls: ['./roster.component.css'],
   providers: [],
-  imports: [],
+  imports: [CommonModule],
   standalone: true,
 })
-export class RosterComponent {}
+export class RosterComponent implements OnInit {
+  userStats: any[] = [];
+
+  constructor(private rosterService: RosterService, private readonly changeDetectorRef: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.rosterService.getUserStats().subscribe((data) => {
+      console.log(data);
+      this.userStats = data;
+      this.changeDetectorRef.markForCheck();
+    });
+  }
+}
